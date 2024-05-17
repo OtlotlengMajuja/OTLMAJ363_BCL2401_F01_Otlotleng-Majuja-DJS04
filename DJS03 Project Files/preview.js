072f3597708254c28d8d3f1c8320817205bdb5e8
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-17 15:22:02 +0200

    Set up the BookPreview class, that uses 'shadow' DOM to encapsulate styles and markup

---
+++ DJS03 Project Files/preview.js
@@ -0,0 +1,49 @@
+import { books, authors } from './data.js';
+
+// Set up the BookPreview class 
+class BookPreview extends HTMLElement {
+    constructor() {
+        super();
+        // Uses 'shadow' DOM to encapsulate styles and markup
+        this.attachShadow({ mode: 'open' });
+        // Retrieves attributes (image, title, author) to populate the preview details
+        this.shadowRoot.innerHTML = `
+            <style>
+                .preview {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    border: 1px solid #ccc;
+                    padding: 10px;
+                    margin: 10px;
+                    cursor: pointer;
+                }
+                .preview__image {
+                    width: 100px;
+                    height: 150px;
+                    object-fit: cover;
+                }
+                .preview__info {
+                    text-align: center;
+                }
+                .preview__title {
+                    font-size: 1.2em;
+                    margin: 10px 0;
+                }
+                .preview__author {
+                    font-size: 0.9em;
+                    color: #555;
+                }
+            </style>
+            <button class="preview">
+                <img class="preview__image" />
+                <div class="preview__info">
+                    <h3 class="preview__title"></h3>
+                    <div class="preview__author"></div>
+                </div>
+            </button>
+        `;
+    }
+
+}
+
